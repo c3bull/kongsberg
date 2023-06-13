0a56f433a04a51c1e9c6f2d46fda67b50f40e2d6
c3bull <kubcor1@gmail.com>
2023-06-13 12:07:04 +0200

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0b0d0f',
+        secondary: '#0F1114',
+      },
+    },
+    backgroundImage: {
+      'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      'gradient-linear': 'linear-gradient(var(--tw-gradient-stops))',
+      'gradient-to-r': 'linear-gradient(to right, var(--tw-gradient-stops))',
+      'gradient-to-b': 'linear-gradient(to bottom, var(--tw-gradient-stops))',
+      'gradient-to-t': 'linear-gradient(to top, var(--tw-gradient-stops))',
+    }
+  },
+  plugins: [],
+}
